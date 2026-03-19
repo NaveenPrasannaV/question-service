2d3f3f1cecb96ea6af29ba1460d1587b857f16ed
Naveen Prasanna <naveenprasannavnp@gmail.com>
2026-03-19 15:44:52 +0530

    api updated

---
+++ src/main/java/org/nit/questionservice/dao/QuestionDao.java
@@ -0,0 +1,22 @@
+package org.nit.questionservice.dao;
+
+import org.nit.questionservice.model.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface QuestionDao extends JpaRepository<Question, Integer> {
+
+    List<Question> findByCategoryIgnoreCase(String category);
+
+    @Query(value =
+            "SELECT q.id FROM question q WHERE LOWER(q.category) = LOWER(:category) ORDER BY RANDOM() LIMIT :numQ",
+            nativeQuery = true)
+    List<Integer> findRandomQuestionsByCategory(@Param("category") String category,
+                                                 @Param("numQ") int numQ);
+
+}
