2db6d3f21cc7e5df5cbda889d4e65d7d9d69c5f5
Naveen Prasanna <naveenprasannavnp@gmail.com>
2026-03-20 13:47:44 +0530

    api updated

--- src/main/java/org/nit/questionservice/controller/QuestionController.java
+++ src/main/java/org/nit/questionservice/controller/QuestionController.java
@@ -5,6 +5,7 @@ import org.nit.questionservice.model.QuestionWrapper;
 import org.nit.questionservice.model.Response;
 import org.nit.questionservice.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +17,9 @@ public class QuestionController {
 
     @Autowired
     QuestionService questionService;
+
+    @Autowired
+    Environment environment;
 
     @GetMapping("allQuestions")
     public ResponseEntity<List<Question>> getAllQuestion() {
@@ -39,6 +43,7 @@ public class QuestionController {
 
     @PostMapping("getQuestions")
     public ResponseEntity<List<QuestionWrapper>> getQuestionsFromId(@RequestBody List<Integer> questionIds) {
+        System.out.println(environment.getProperty("local.server.port"));
         return questionService.getQuestionsFromId(questionIds);
     }
 
